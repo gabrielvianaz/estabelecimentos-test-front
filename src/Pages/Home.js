9e013ec2092cf269cb5097f4f3be0f68059b0797
Gabriel Viana <gabrielvianagvm@gmail.com>
2022-11-14 00:53:34 -0300

    mudanca na home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 import Estabelecimentos from '../Components/Home/Estabelecimentos';
 import { Animated } from 'react-animated-css';
 import { useNavigate } from 'react-router-dom';
+import styles from '../Assets/Styles/Home.module.css';
+import Spinner from '../Components/Spinner';
 
 const Home = () => {
   const [tokenOk, setTokenOk] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const navigate = useNavigate();
 
   React.useEffect(() => {
     if (localStorage.token) {
+      setLoading(true);
       axios
         .post(
           'https://estabelecimentos-back.herokuapp.com/verificar',
@@ -20,7 +24,10 @@ const Home = () => {
             },
           }
         )
-        .then(() => setTokenOk(true))
+        .then(() => {
+          setLoading(false);
+          setTokenOk(true);
+        })
         .catch(() => {
           localStorage.clear();
           navigate('/login');
@@ -32,6 +39,14 @@ const Home = () => {
 
   return (
     <>
+      {loading && (
+        <div className={styles.container}>
+          <h1 className="tituloVerde">Meus estabelecimentos</h1>
+          <div className={styles.spinner}>
+            <Spinner />
+          </div>
+        </div>
+      )}
       {tokenOk && (
         <Animated>
           <Estabelecimentos />
